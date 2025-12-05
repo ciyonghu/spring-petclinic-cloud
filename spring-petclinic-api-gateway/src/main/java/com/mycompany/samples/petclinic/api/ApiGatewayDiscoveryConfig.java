b01ee5c056d9300427874daf27b669c350a15346
ciyonghu <18395969+ciyonghu@users.noreply.github.com>
2025-12-05 09:59:37 +0000

    Refactor: Rename packages using OpenRewrite

---
+++ spring-petclinic-api-gateway/src/main/java/com/mycompany/samples/petclinic/api/ApiGatewayDiscoveryConfig.java
@@ -0,0 +1,12 @@
+package com.mycompany.samples.petclinic.api;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+//@Profile("!cloud")
+@Configuration
+@EnableDiscoveryClient
+public class ApiGatewayDiscoveryConfig {
+
+}
---
+++ spring-petclinic-api-gateway/src/test/java/com/mycompany/samples/petclinic/api/ApiGatewayApplicationTests.java
@@ -0,0 +1,15 @@
+package com.mycompany.samples.petclinic.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
